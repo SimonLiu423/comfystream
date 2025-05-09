9eec993c06f12f0d7dd7342d0555508cd5a4a978
SimonLiu <simonliu423@gmail.com>
2025-05-09 14:32:59 +0800

    Feat: replace pipeline.py

--- entrypoint_wrapper.sh
+++ entrypoint_wrapper.sh
@@ -4,9 +4,9 @@ set -e
 cd /workspace/unity-cs
 git pull origin main -f
 cp -r /workspace/unity-cs/server /workspace/comfystream
+cp /workspace/unity-cs/src/comfystream/pipeline.py /workspace/comfystream/src/comfystream/pipeline.py
 cp /workspace/unity-cs/nodes/api/__init__.py /workspace/comfystream/nodes/api/__init__.py
 
 cd /workspace/comfystream
 
 exec "$@"
-
--- entrypoint_wrapper_dev.sh
+++ entrypoint_wrapper_dev.sh
@@ -5,6 +5,7 @@ cd /workspace/unity-cs
 git checkout dev
 git pull origin dev -f
 cp -r /workspace/unity-cs/server /workspace/comfystream
+cp /workspace/unity-cs/src/comfystream/pipeline.py /workspace/comfystream/src/comfystream/pipeline.py
 cp /workspace/unity-cs/nodes/api/__init__.py /workspace/comfystream/nodes/api/__init__.py
 
 cd /workspace/comfystream
